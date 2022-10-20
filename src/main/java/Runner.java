88eced45df8be1d1c3471f715cffc1a18ffbbade
justiwannauseit <semlyovich@yandex.ru>
2022-10-20 21:31:31 +0300

    fix property name

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
     public static void main(final String... args) {
 
         var from = System.getProperty("from", "./allure-results");
-        var out = System.getProperty("from", "out.docx");
+        var out = System.getProperty("out", "out.docx");
 
         var results = ResultHelper.getTestResults(new File(from).toPath());
         DocxHelper.saveDocument(DocxHelper.createDocxFile(results), out);
