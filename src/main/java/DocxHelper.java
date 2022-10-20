f0dd38b8d023d4dfbab4e92db571297e328dae1b
justiwannauseit <semlyovich@yandex.ru>
2022-10-20 20:52:00 +0300

    creator

--- src/main/java/DocxHelper.java
+++ src/main/java/DocxHelper.java
@@ -1,29 +1,35 @@
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StepResult;
+import io.qameta.allure.model.TestResult;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+import metric.Metric;
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @UtilityClass
 public class DocxHelper {
 
     @SneakyThrows
-    public XWPFDocument createDocxFile(Map<String, String> res) {
+    public XWPFDocument createDocxFile(List<TestResult> results) {
+
+        //Получим метрики
+        var metric = new Metric(results);
+        //Сохраним круговую диограмму результатов
+        PieChartGenerator.save(metric);
+
         XWPFDocument document = new XWPFDocument();
-        var temp = new XWPFDocument(new FileInputStream("./temp.docx"));
 
         // Создание заголовка
         XWPFParagraph title = document.createParagraph();
         title.setAlignment(ParagraphAlignment.CENTER);
-
-
-        System.out.println(title.getStyle());
 
         // Фрматирование заголовка
         XWPFRun titleRun = title.createRun();
@@ -33,18 +39,15 @@ public class DocxHelper {
         titleRun.setFontFamily("Times New Roman");
         titleRun.setFontSize(32);
 
-//        document.createStyles();
-//        document.createStyles().addStyle(temp.getStyles().getStyle("Heading1"));
-//        document.createStyles().addStyle(temp.getStyles().getStyle("Heading2"));
-//        titleRun.setStyle("Heading1");
-
-
         // Подзаголовок
         XWPFParagraph subTitle = document.createParagraph();
         subTitle.setAlignment(ParagraphAlignment.CENTER);
         // Фотрматирование подзаголовка
         XWPFRun subTitleRun = subTitle.createRun();
-        subTitleRun.setText("Результаты тестирования за 10.10.2022");
+        var date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+
+        subTitleRun.setText("Результаты тестирования за " + date.format(formatter));
         subTitleRun.setColor("000000");
         subTitleRun.setFontFamily("Times New Roman");
         subTitleRun.setFontSize(16);
@@ -62,12 +65,29 @@ public class DocxHelper {
         var imagePath = new File("./output_image.png").toPath();
         imageRun.addPicture(Files.newInputStream(imagePath),
                 XWPFDocument.PICTURE_TYPE_PNG, imagePath.getFileName().toString(),
-                Units.toEMU(450), Units.toEMU(450));
+                Units.toEMU(350), Units.toEMU(350));
 
+        printMetrics(document, metric);
 
         //Создадим таблицу
-//        var map = Map.of("Проверка работы страницы авторизации", "PASSED", "Авторизация в системе под ролью Иван", "FAILED");
-        generateTable2X2FromMap(document, res);
+        generateTable2X2FromMap(document, results);
+
+        /**
+         * Напишем, что дальше пойдут результаты:
+         * */
+        // Создание заголовка
+        XWPFParagraph title2 = document.createParagraph();
+        title2.setAlignment(ParagraphAlignment.LEFT);
+        // Фрматирование заголовка
+        XWPFRun titleRun2 = title2.createRun();
+        titleRun2.setText("Тест кейсы");
+        titleRun2.setColor("000000");
+        titleRun2.setBold(true);
+        titleRun2.setFontFamily("Times New Roman");
+        titleRun2.setFontSize(32);
+
+        //Печатаем сами шаги
+        results.forEach(result -> printSteps(document, result));
 
         return document;
     }
@@ -81,27 +101,42 @@ public class DocxHelper {
         return true;
     }
 
-    private static void generateTable2X2FromMap(XWPFDocument document, Map<String, String> map) {
-        XWPFTable table = document.createTable(map.size() + 1, 2);
+    private static void printMetrics(XWPFDocument document, Metric metric) {
+        XWPFParagraph paragraph = document.createParagraph();
+        paragraph.setAlignment(ParagraphAlignment.CENTER);
+        printCurrentMetric(paragraph, "Всего: " + metric.getCountAllTests() + "; ");
+        printCurrentMetric(paragraph, "Успешных: " + metric.getCountPassedTests() + "; ");
+        printCurrentMetric(paragraph, "Проваленных: " + metric.getCountFailedTests() + "; ");
+        printCurrentMetric(paragraph, "Сломанных: " + metric.getCountBrokenTests() + "; ");
+        printCurrentMetric(paragraph, "Пропущенных: " + metric.getCountSkippedTests() + "; ");
+        printCurrentMetric(paragraph, "Неизвестных: " + metric.getCountUnknownTests() + ".");
+    }
 
-        //Получим список заголовки
-        var keys = map.keySet().stream().collect(Collectors.toList());
-        var values = map.values().stream().collect(Collectors.toList());
+    private static void printCurrentMetric(XWPFParagraph paragraph, String text) {
+        XWPFRun paragraphRun = paragraph.createRun();
+        paragraphRun.setText(text);
+        paragraphRun.setColor("000000");
+        paragraphRun.setFontFamily("Times New Roman");
+        paragraphRun.setFontSize(12);
+        paragraphRun.setTextPosition(20);
+        paragraphRun.setBold(true);
+    }
+
+    private static void generateTable2X2FromMap(XWPFDocument document, List<TestResult> results) {
+        XWPFTable table = document.createTable(results.size() + 1, 2);
 
         createParagraphForTableTitle(table.getRow(0).getCell(0).getParagraphs().get(0), "Тест кейс");
         createParagraphForTableTitle(table.getRow(0).getCell(1).getParagraphs().get(0), "Статус");
 
         //Добавим Заголоки тест кейсов
-        for (int i = 0; i < map.size(); i++) {
-            createParagraphForTableBodyTestCaseName(table.getRow(i + 1).getCell(0).getParagraphs().get(0), keys.get(i));
-        }
+        for (int i = 0; i < results.size(); i++) {
+            createParagraphForTableBodyTestCaseName(table.getRow(i + 1).getCell(0).getParagraphs().get(0), results.get(i).getFullName());
 
-        //Добавим Статусы тест кейсов
-        for (int i = 0; i < map.size(); i++) {
-            if (values.get(i).contains("FAILED")) {
+            var status = results.get(1).getStatus();
+            if (status.equals(Status.FAILED)) {
                 table.getRow(i + 1).getCell(1).setColor("facc9d");
             }
-            createParagraphForTableBodyStatus(table.getRow(i + 1).getCell(1).getParagraphs().get(0), values.get(i));
+            createParagraphForTableBodyStatus(table.getRow(i + 1).getCell(1).getParagraphs().get(0), status.value());
         }
     }
 
@@ -141,6 +176,67 @@ public class DocxHelper {
         paragraphRun.setFontSize(12);
 
         return paragraph;
+    }
+
+    private void printSteps(XWPFDocument document, TestResult result) {
+        //Это мы только заголовок напечатали и статус
+
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.LEFT);
+        // Фотрматирование подзаголовка
+        XWPFRun subTitleRun = title.createRun();
+        subTitleRun.setText("\n" + result.getFullName());
+        subTitleRun.setColor("000000");
+        subTitleRun.setFontFamily("Times New Roman");
+        subTitleRun.setFontSize(14);
+        subTitleRun.setTextPosition(0);
+        subTitleRun.setBold(true);
+
+        var statusColor = title.createRun();
+        setColorByStatus(result.getStatus(), statusColor);
+        statusColor.setFontFamily("Times New Roman");
+        statusColor.setFontSize(14);
+        statusColor.setTextPosition(0);
+        statusColor.setBold(true);
+        statusColor.setText(" ► ");
+
+        var status = title.createRun();
+        status.setColor("000000");
+        status.setFontFamily("Times New Roman");
+        status.setFontSize(14);
+        status.setTextPosition(0);
+        status.setBold(true);
+        status.setText(result.getStatus().name());
+
+        //А теперь уже напечатем сами шаги:
+        result.getSteps().forEach(x -> printRealSteps(document, x));
+    }
+
+    private void printRealSteps(XWPFDocument document, StepResult stepResult) {
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.LEFT);
+        XWPFRun subTitleRun = title.createRun();
+        setColorByStatus(stepResult.getStatus(), subTitleRun);
+        subTitleRun.setText("[" + stepResult.getStatus() + "] " + stepResult.getName());
+        subTitleRun.setFontFamily("Times New Roman");
+        subTitleRun.setFontSize(12);
+        subTitleRun.setTextPosition(20);
+
+    }
+
+    private void setColorByStatus(Status status, XWPFRun subTitleRun) {
+        if (status.equals(Status.FAILED)) {
+            subTitleRun.setColor("FF0000");
+        }
+        if (status.equals(Status.SKIPPED)) {
+            subTitleRun.setColor("6e6e6e");
+        }
+        if (status.equals(Status.PASSED)) {
+            subTitleRun.setColor("000000");
+        }
+        if (status.equals(Status.BROKEN)) {
+            subTitleRun.setColor("d0d40b");
+        }
     }
 
 }
--- src/main/java/PieChartGenerator.java
+++ src/main/java/PieChartGenerator.java
@@ -1,8 +1,10 @@
 import lombok.SneakyThrows;
+import metric.Metric;
 import org.knowm.xchart.PieChart;
 import org.knowm.xchart.PieChartBuilder;
 import org.knowm.xchart.PieSeries;
 import org.knowm.xchart.style.Styler;
+import style.Colors;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -11,31 +13,23 @@ import java.io.File;
 
 public class PieChartGenerator {
 
-    public static PieChart getChart(long countPassedTests,
-                                    long countFailedTests,
-                                    long countBrokenTests,
-                                    long countSkippedTests,
-                                    long countUnknownTests) {
+    public static PieChart getChart(Metric metric) {
 
         // Create Chart
-        PieChart chart = new PieChartBuilder().width(800).height(800).title("Результаты").build();
+        PieChart chart = new PieChartBuilder()
+                .width(800)
+                .height(800)
+                .title(String.format("Из %d тестов успешных %d", metric.getCountAllTests(), metric.getCountPassedTests()))
+                .build();
 
         // Customize Chart
-        Color[] sliceColors = new Color[]{
-                new Color(34, 197, 94),
-                new Color(239, 68, 68),
-                new Color(250, 204, 21),
-                new Color(148, 163, 184),
-                new Color(168, 85, 247)};
-
-        var emptyColor = new Color(255, 255, 255, 0);
-
-        chart.getStyler().setSeriesColors(sliceColors);
+        chart.getStyler().setSeriesColors(Colors.getSliceColors());
 
         chart.getStyler().setChartTitleVisible(false);
-        chart.getStyler().setChartTitleFont(new Font("Century Gothic", 1, 20));
+        chart.getStyler().setChartTitleFont(new Font("Times New Roman", 0, 20));
+
         chart.getStyler().setLabelsFont((new Font("Century Gothic", 1, 20)));
-        chart.getStyler().setLabelsFontColor(new Color(232, 229, 229));
+        chart.getStyler().setLabelsFontColor(Colors.WHITE.getColor());
 
         chart.getStyler().setChartBackgroundColor(Color.white);
 
@@ -45,34 +39,29 @@ public class PieChartGenerator {
         chart.getStyler().setLabelsDistance(0.82);
         chart.getStyler().setPlotContentSize(.80);
         chart.getStyler().setDefaultSeriesRenderStyle(PieSeries.PieSeriesRenderStyle.Donut);
-        chart.getStyler().setPlotBorderColor(emptyColor);
+        chart.getStyler().setPlotBorderColor(Colors.EMPTY.getColor());
 
         chart.getStyler().setLegendPadding(15);
-        chart.getStyler().setLegendFont(new Font("Century Gothic", 1, 15));
+        chart.getStyler().setLegendFont(new Font("Century Gothic", 1, 22));
         chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSE);
-        chart.getStyler().setLegendBorderColor(emptyColor);
+        chart.getStyler().setLegendBorderColor(Colors.EMPTY.getColor());
 
         chart.getStyler().setSliceBorderWidth(3);
 
         // Series
-        chart.addSeries("Успешные", countPassedTests);
-        chart.addSeries("Не успешные", countFailedTests);
-        chart.addSeries("Ошибка теста", countBrokenTests);
-        chart.addSeries("Пропущенные", countSkippedTests);
-        chart.addSeries("Неизвестные", countUnknownTests);
+        chart.addSeries(Colors.PASSED.getLabel(), metric.getCountPassedTests());
+        chart.addSeries(Colors.FAILED.getLabel(), metric.getCountFailedTests());
+        chart.addSeries(Colors.BROKEN.getLabel(), metric.getCountBrokenTests());
+        chart.addSeries(Colors.SKIPPED.getLabel(), metric.getCountSkippedTests());
+        chart.addSeries(Colors.UNKNOWN.getLabel(), metric.getCountUnknownTests());
 
         return chart;
     }
 
 
     @SneakyThrows
-    public static void save(long countPassedTests,
-                            long countFailedTests,
-                            long countBrokenTests,
-                            long countSkippedTests,
-                            long countUnknownTests) {
-        var pieChart = getChart(countPassedTests, countFailedTests, countBrokenTests, countSkippedTests, countUnknownTests);
-
+    public static void save(Metric metric) {
+        var pieChart = getChart(metric);
         BufferedImage bImg = new BufferedImage(pieChart.getWidth(), pieChart.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = bImg.createGraphics();
         graphics2D.setColor(Color.BLUE);
